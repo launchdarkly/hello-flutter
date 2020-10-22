5b456624ebb80c9bb077945e7754e3c74ebd977e
Gavin Whelan <gwhelan@launchdarkly.com>
2020-10-22 12:16:26 -0700

    Hello flutter initial commit.

---
+++ README.md
@@ -0,0 +1,17 @@
+# LaunchDarkly Sample Flutter Application
+
+We've built a simple Flutter application that demonstrates how LaunchDarkly's SDK works.
+
+Below, you'll find the basic build procedure, but for more comprehensive instructions, you can visit the [Flutter SDK reference guide](https://docs.launchdarkly.com/sdk/client-side/flutter).
+
+## Build instructions
+
+1. Make sure you have [Flutter SDK](https://flutter.dev/docs/get-started/install) installed.
+
+2. Edit `lib/main.dart` and set the value of `MOBILE_SDK_KEY` to your LaunchDarkly mobile key. If there is an existing boolean feature flag in your LaunchDarkly project that you want to evaluate, set `FEATURE_FLAG_KEY` to the flag key.
+
+3. Open an iOS simulator instance, Android emulator instance, or connect a physical device to run the application on.
+
+4. On the command line, run `flutter run` to start the application on the device.
+
+5. The application will show the value of the flag `FEATURE_FLAG_KEY` in the UI.
---
+++ lib/main.dart
@@ -0,0 +1,57 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+import 'package:launchdarkly_flutter_client_sdk/launchdarkly_flutter_client_sdk.dart';
+
+const String MOBILE_SDK_KEY = '';
+const String FEATURE_FLAG_KEY = 'YOUR_FEATURE_KEY';
+
+void main() async {
+  runApp(HelloApp());
+}
+
+class HelloApp extends StatefulWidget {
+  @override _HelloAppState createState() => _HelloAppState();
+}
+
+class _HelloAppState extends State<HelloApp> {
+  final String title = 'LaunchDarkly Hello Flutter';
+  String message = 'Initializing';
+
+  @override
+  Future<void> initState() {
+    super.initState();
+    if (MOBILE_SDK_KEY != '') {
+      final LDConfig config = LDConfigBuilder(MOBILE_SDK_KEY).build();
+      final LDUser user = LDUserBuilder('user key').build();
+      LaunchdarklyFlutterClientSdk.start(config, user).whenComplete(() {
+          updateFlagEvaluation();
+          LaunchdarklyFlutterClientSdk.registerFeatureFlagListener(FEATURE_FLAG_KEY, updateFlagEvaluation);
+      });
+    } else {
+      setState(() { message = 'MOBILE_SDK_KEY not set'; });
+    }
+  }
+
+  Future<void> updateFlagEvaluation([String flagKey]) async {
+    var result = await LaunchdarklyFlutterClientSdk.boolVariation(FEATURE_FLAG_KEY, false);
+    setState(() { message = 'Feature "$FEATURE_FLAG_KEY" evaluated to ' + result.toString(); });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: title,
+      theme: ThemeData.light(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(title),
+        ),
+        body: Center(
+          child: Text(message),
+        ),
+      ),
+    );
+  }
+}
